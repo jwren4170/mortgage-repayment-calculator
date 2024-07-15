cf0e9a2a47eeac6aa525c02c13c8b8283d90e209
jwren4170 <jwren4170@att.net>
2024-07-15 07:21:18 -0500

    Add custom tailwind styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"; 
 
 const config: Config = {
   content: [
@@ -8,11 +8,24 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+       'lime': 'hsl(61, 70%, 52%)',
+       'red': 'hsl(4, 69%, 50%)',
+       'white': 'hsl(0, 0%, 100%)',
+       'slate-100': 'hsl(202, 86%, 94%)',
+       'slate-300': 'hsl(203, 41%, 72%)',
+       'slate-500': 'hsl(200, 26%, 54%)',
+       'slate-700': 'hsl(200, 24%, 40%)',
+       'slate-900': 'hsl(202, 55%, 16%)',
       },
+      fontWeight: {
+        'custom-500': '500',
+        'custom-700': '700'
+      },
+      screens: {
+        'mobile': '23rem',
+        'desktop': '90rem'
+      }
     },
   },
   plugins: [],
